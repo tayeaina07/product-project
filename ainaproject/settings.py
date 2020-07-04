83ae1aa11494f8346284681e7241fa4a769544b4
tayeaina007 <taye1232002@yahoo.com>
2020-07-04 15:46:17 +0100

    My first commit

--- ainaproject/settings.py
+++ ainaproject/settings.py
@@ -131,3 +131,6 @@ os.path.join(BASE_DIR, 'ainaproject/static/')
 
 STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
