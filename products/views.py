3b7ca45b993e153bbffb176f0c4bd4a38335f283
tayeaina007 <taye1232002@yahoo.com>
2020-07-06 09:18:58 +0100

    polished site

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
@@ -14,7 +15,10 @@ def create(request):
             product = Product()
             product.title = request.POST['title']
             product.body = request.POST['body']
-            
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
             product.icon = request.FILES['icon']
             product.image = request.FILES['image']
             product.pub_date = timezone.datetime.now()
@@ -30,10 +34,10 @@ def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'products/detail.html',{'product':product})
 
-
 @login_required(login_url="/accounts/signup")
 def upvote(request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
-    product.votes_total += 1
-    product.save()
-    return redirect('/products/' + str(product.id))
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/' + str(product.id))
